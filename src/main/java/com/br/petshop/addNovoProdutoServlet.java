9e92dc697b0b5b7d7a2b65dc30a2f4e8f390f69d
Victor Casagrande <victorcasag@hotmail.com>
2021-10-02 23:20:40 -0300

    commit

--- src/main/java/com/br/petshop/addNovoProdutoServlet.java
+++ src/main/java/com/br/petshop/addNovoProdutoServlet.java
@@ -20,12 +20,17 @@ public class addNovoProdutoServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String nome = request.getParameter("nomeProduto");
 		float preco = Float.parseFloat(request.getParameter("preco"));
-
+		String marca = request.getParameter("marca");
+		String descricao = request.getParameter("descricao");
+		int qntidade = Integer.parseInt(request.getParameter("qntidade"));
 		
-		Produto servico = new Produto();
+		Produto produto = new Produto();
 		produto.setNome(nome);
 		produto.setPreco(preco);
-
+		produto.setMarca(marca);
+		produto.setDescricao(descricao);
+		produto.setQntidade(qntidade);
+		
 		request.getSession().setAttribute("produto", produto);
 		
 		
--- src/main/java/com/br/petshop/listarProdutosServlet.java
+++ src/main/java/com/br/petshop/listarProdutosServlet.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
 
-import com.br.petshop.Objs.Cliente;
+import com.br.petshop.Objs.Produto;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,21 +12,24 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.ServletException;
 
-@WebServlet(name = "listarClientes", urlPatterns = { "/listarClientes" })
-public class listarClientesServlet extends HttpServlet {
+@WebServlet(name = "listarProdutos", urlPatterns = { "/listarProdutos" })
+public class listarProdutosServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		PrintWriter out = response.getWriter();
 		
-		Cliente ss = (Cliente)request.getSession().getAttribute("cliente");
+		Produto ss = (Produto)request.getSession().getAttribute("produto");
 
 		out.println("<html><body>");
 
+		System.out.print(ss.getId());
 		System.out.print(ss.getNome());
-		System.out.print(ss.getEndereco());
-		System.out.print(ss.getTelefone());
+		System.out.print(ss.getDescricao());
+		System.out.print(ss.getMarca());
+		System.out.print(ss.getPreco());
+		System.out.print(ss.getQntidade());
 		
 		out.println("</body></html>");
 	}
